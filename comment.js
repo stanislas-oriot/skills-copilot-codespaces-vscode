891118b9f9a02bca4f428cb5a05cdd06a6b6fa34
stanislas-oriot <142139779+stanislas-oriot@users.noreply.github.com>
2023-08-13 08:13:16 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,12 +1,22 @@
-// Create web server
-// 1. npm install express
-// 2. npm install body-parser
+// create web server
+// 1. create web server
+// 2. static web server
+// 3. dynamic web server
+// 4. database
+// 5. MVC
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-const fs = require('fs');
-
-// body-parser
-app.use(bodyParser.urlencoded({ extended: false }
+// 1. create web server
+// 1.1 import http module
+var http = require('http');
+// 1.2 create web server
+var server = http.createServer();
+// 1.3 listen request event
+server.on('request',function(req,res){
+    // 1.3.1 response
+    res.write('hello');
+    res.end();
+});
+// 1.4 listen port
+server.listen(3000,function(){
+    console.log('server is running...');
+});
